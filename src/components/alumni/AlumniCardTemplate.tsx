0d97a3ccace85fa629ebba5f796d3519e09460ce
Uzair Shafqat <uzair.works3@gmail.com>
2026-03-27 16:21:01 +0500

    increased card width

--- src/components/alumni/AlumniCardTemplate.tsx
+++ src/components/alumni/AlumniCardTemplate.tsx
@@ -141,7 +141,7 @@ export default function AlumniCardTemplate({
       <div 
         className="relative overflow-hidden rounded-lg shadow-md"
         style={{
-          width: "321.26px",
+          width: "323.26px",
           height: "212.03px",
         }}
       >
